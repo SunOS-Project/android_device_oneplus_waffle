e334097bf7fbc946ead3a3ebbb5a6f0398474c47
dianlujitao <dianlujitao@lineageos.org>
2025-12-28 10:02:45 +0000

    waffle: Build AOSP NXP keymint HAL
    Change-Id: Ic428a37e49a77cc6f96f1209f687bfca8c397ab5

--- device.mk
+++ device.mk
@@ -21,6 +21,11 @@ TARGET_SCREEN_WIDTH := 1440
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946450791512195.xml
 
+# Keymint
+PRODUCT_PACKAGES += \
+    android.hardware.security.keymint3-service.strongbox.nxp \
+    android.hardware.weaver-service.nxp
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
